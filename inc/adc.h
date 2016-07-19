ad41b762b1394d0057c2b27af6ac0f09daee3c9e
Tristan Nixon <Tristan Nixon>
2016-07-19 12:02:30 -0700

    Added comments to function prototype

--- inc/adc.h
+++ inc/adc.h
@@ -12,6 +12,24 @@
 #include "stm32f4xx.h"
 #include "stm32f4xx_adc.h"
 
+/*
+ * 	void ADC3_CH12_Config(void)
+ * 	Author: Tristan
+ *
+ * 	This function initializes ADC3 on the stm32f4 using channel 12.
+ * 	The function enables the RCC timers for both the ADc peripheral
+ * 	as well as the GPIO pin associated with the channel selected. The
+ * 	NVIC global interrupts for the ADC are also enabled here.
+ *
+ * 	Peripheral:	ADC3, CH12
+ * 	Pin:		PC2
+ * 	Mode:		Independent
+ * 				prescaler/2
+ * 				DMA Disabled
+ * 				Two Sample Delay
+ * 				Left justified
+ * 				EOC interrupt enabled
+ */
 void ADC3_CH12_Config(void);
 
 #endif /* ADC_H_ */
