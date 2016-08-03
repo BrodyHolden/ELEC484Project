3acde94a851c34f0704376cba7b8183e19efdc93
Ann-Kristin <Ann-Kristin>
2016-08-03 13:16:39 -0700

    sparated windows into real and imaginary parts

--- MatLAB/brute_force_window_separation.m
+++ MatLAB/brute_force_window_separation.m
@@ -1,14 +1,61 @@
 window_1 = out(1:N);
+
+window_1_real = real(window_1);
+window_1_imag = imag(window_1);
+
 window_2 = out(1*N+1:N*2);
+
+window_2_real = real(window_2);
+window_2_imag = imag(window_2);
+
 window_3 = out(2*N+1:N*3);
+
+window_3_real = real(window_3);
+window_3_imag = imag(window_3);
+
 window_4 = out(3*N+1:N*4);
+
+window_4_real = real(window_4);
+window_4_imag = imag(window_4);
+
 window_5 = out(4*N+1:N*5);
+
+window_5_real = real(window_5);
+window_5_imag = imag(window_5);
+
 window_6 = out(5*N+1:N*6);
+
+window_6_real = real(window_6);
+window_6_imag = imag(window_6);
+
 window_7 = out(6*N+1:N*7);
+
+window_7_real = real(window_7);
+window_7_imag = imag(window_7);
+
 window_8 = out(7*N+1:N*8);
+
+window_8_real = real(window_8);
+window_8_imag = imag(window_8);
+
 window_9 = out(8*N+1:N*9);
+
+window_9_real = real(window_9);
+window_9_imag = imag(window_9);
+
 window_10 = out(9*N+1:N*10);
+
+window_10_real = real(window_10);
+window_10_imag = imag(window_10);
+
 window_11 = out(10*N+1:N*11);
+
+window_11_real = real(window_11);
+window_11_imag = imag(window_11);
+
 window_12 = out(11*N+1:N*12);
+
+window_12_real = real(window_12);
+window_12_imag = imag(window_12);
 
 
