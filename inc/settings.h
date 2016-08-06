dba16c01e77340e2958ea038619b63bda5b47e1c
Brody Holden <brody.holden.r@gmail.com>
2016-08-05 18:37:40 -0700

    Add static_assert that uint16_t can index all of buffer of size INPUT_BUFFER_SIZE

--- inc/settings.h
+++ inc/settings.h
@@ -8,14 +8,16 @@
 #ifndef SETTINGS_H_
 #define SETTINGS_H_
 
+#include <stdint.h>
+#include <assert.h>
+
 // 512 real values interlaced by 512 complex values.
 // Real values in even position. Complex values in odd position.
 #define WINDOW_SIZE (512 * 2)
 
 #define INPUT_BUFFER_SIZE (WINDOW_SIZE * 8)
 
-#include <assert.h>
-
 static_assert(INPUT_BUFFER_SIZE % 2 == 0, "INPUT_BUFFER_SIZE must be even.");
+static_assert(INPUT_BUFFER_SIZE < UINT16_MAX, "Buffer is too large to for uint16_t to index it.");
 
 #endif /* SETTINGS_H_ */
