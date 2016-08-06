b44b99f44dbc36f4176eb6bf85858d15badca1cc
Brody Holden <brody.holden.r@gmail.com>
2016-08-05 18:13:15 -0700

    Add static_assert that INPUT_BUFFER_SIZE is even.

--- inc/settings.h
+++ inc/settings.h
@@ -9,8 +9,13 @@
 #define SETTINGS_H_
 
 // 512 real values interlaced by 512 complex values.
+// Real values in even position. Complex values in odd position.
 #define WINDOW_SIZE (512 * 2)
 
 #define INPUT_BUFFER_SIZE (WINDOW_SIZE * 8)
+
+#include <assert.h>
+
+static_assert(INPUT_BUFFER_SIZE % 2 == 0, "INPUT_BUFFER_SIZE must be even.");
 
 #endif /* SETTINGS_H_ */
